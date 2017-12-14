78ac0c3d12442a0e98d63766556a546a88c9978e
psarma89 <priyam.sarma@gmail.com>
2017-12-13 22:16:49 -0500

    Done.

--- crewMember.js
+++ crewMember.js
@@ -13,24 +13,24 @@ class CrewMember{
   //   }
   // }
   engageWarpDrive(){
-    if (this.position === "Pilot") {
+    if (this.position === "Pilot" && this.currentShip !=="Looking for a Rig") {
       return this.currentShip.warpDrive = 'engaged!';
     }else {
       return 'had no effect'
     }
   }
   setsInvisibility(){
-    if (this.position === "Defender") {
+    if (this.position === "Defender" && this.currentShip !=="Looking for a Rig") {
       return this.currentShip.cloaked = true;
     }else {
       return 'had no effect'
     }
   }
   chargePhasers(){
-    if (this.position === "Gunner") {
+    if (this.position === "Gunner" && this.currentShip !=="Looking for a Rig") {
       return this.currentShip.phasersCharge = 'charged!'
     }else {
-      return 'had no effect'      
+      return 'had no effect'
     }
   }
 
